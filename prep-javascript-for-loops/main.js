f55434e674ddd8b1a8ce6fd936eaa229998473e0
Christine Tina Park <escp1004jk@gmail.com>
2021-08-12 15:56:05 -0700

    Completed prep-javascript-for-loops. - Christine Park

---
+++ prep-javascript-for-loops/main.js
@@ -0,0 +1,12 @@
+for (let i = 0; i < 10; i++) {
+  console.log(i);
+}
+
+var x = 2;
+for (let j = 0; j < 10; j++) {
+  console.log(x += 2);
+}
+
+for (let k = 100; k >= 0; k--) {
+  console.log('Time till explosion: ' + k);
+}
